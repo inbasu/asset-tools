0c2794d95e3b9b6a28818fdd53cefceed9e72d74
Inasu <just.me.juncy@gmail.com>
2024-06-27 23:56:47 +0300

    modified:   utils/views.py

--- utils/views.py
+++ utils/views.py
@@ -1,10 +1,48 @@
-from django.http import HttpResponse
+# from email.mime.application import MIMEMultipart
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from django.http import FileResponse
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from users.permissions import ItUserPermission, UserPermission
 
-# Create your views here.
-class DownloadMoblieBlank(APIView):
+""" Every view can be a mictodervise in future"""
+
+
+class DownloadMoblieBlankView(APIView):
     http_method_names = ["post"]
 
     def post(self, request):
-        return HttpResponse()
+        return FileResponse()
+
+
+class DownloadExcelReportView(APIView):
+    http_method_names = ["post"]
+    renderer_classes = [ItUserPermission | UserPermission]
+
+    def post(self, request):
+        return FileResponse()
+
+
+class SendMailView(APIView):
+    http_method_names = ["post"]
+    permission_classes = [ItUserPermission | UserPermission]
+
+    def post(self, request):
+        msg = MIMEMultipart()
+        msg["Subject"] = request.data.get("subject", "")
+        msg["From"] = f"<{request}>"  # request user session user
+        msg["Cc"] = request.data.get("copy", "")
+        body = MIMEText(request.data.get("body", ""))
+        msg.attach(request.data.get(body, ""))
+        for file in request.FILES:
+            msg.attach(file)
+        if msg["To"] is not None:
+            with smtplib.SMTP(host="") as connection:
+                connection.starttls()
+                connection.sendmail(from_addr=msg["From"], to_addrs=msg["To"], msg=msg.as_stri())
+            return Response({})
+        return Response({})
