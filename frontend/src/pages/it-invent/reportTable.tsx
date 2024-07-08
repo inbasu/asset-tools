8113471867ef018783bcd524d55f8f5a08675379
Inasu <just.me.juncy@gmail.com>
2024-07-08 19:09:44 +0300

    new file:   frontend/src/pages/it-invent/reportTable.tsx

---
+++ frontend/src/pages/it-invent/reportTable.tsx
@@ -0,0 +1,72 @@
+import { Fragment, useEffect, useState } from 'react';
+import { formPlaceReport, formTillReport, Item, Report } from './data';
+
+import Collapse from '@mui/material/Collapse';
+import Table from '@mui/material/Table';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+type params = {
+  report: Report | null;
+  items: Array<Item>;
+  fields: Map<string, boolean>;
+};
+
+type Props = {
+  loc: string;
+  items: Array<Item>;
+  fields: Map<string, boolean>;
+};
+
+function Row({ loc, items, fields }: Props) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Fragment>
+      <TableRow>
+        <TableCell>
+          <IconButton onClick={() => setOpen(!open)}></IconButton>
+          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </TableCell>
+        <TableCell>{loc}</TableCell>
+      </TableRow>
+      <Collapse in={open} unmountOnExit>
+        {items.map((item) => {
+          return (
+            <TableRow sx={{ backgroundColor: item.invented === 'yes' ? '#e8f5e9' : '#ffebee' }}>
+              {[...fields.keys()].map((f) => {
+                if (fields.get(f)) {
+                  return <TableCell>{item[f]}</TableCell>;
+                }
+              })}
+            </TableRow>
+          );
+        })}
+      </Collapse>
+    </Fragment>
+  );
+}
+
+export default function ReportTable({ items, report, fields }: params) {
+  const [reportItems, setItems] = useState<Map<string, Array<Item>>>();
+  useEffect(() => {
+    switch (report?.name) {
+      case 'Кассы':
+        setItems(formTillReport(items));
+        break;
+      case 'По местам':
+        setItems(formPlaceReport(items));
+        break;
+    }
+  }, [report]);
+
+  return (
+    <Table>
+      {(reportItems ? [...reportItems.entries()] : []).map((one) => {
+        return <Row fields={fields} items={one[1]} loc={one[0]} />;
+      })}
+    </Table>
+  );
+}
